8efb65da4f420c7cbaaf178cd7bbabca3fdce875
Ethan-Chew <ethanchewmh@gmail.com>
2024-06-03 00:33:04 +0800

    feat: landing page

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const app = express();
+const staticMiddleware = express.static("public");
+
---
+++ public/scripts/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#47A6BC',
+                btnprimary: '#3C8D9F',
+                btnsecondary: '#E9E9E9',
+                btnsecondary_hover: "#D0D0D0"
+            },
+        },
+    }
+}
