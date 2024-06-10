7429a8f32ae8645799fdf569235a0a8be14917d8
WizardingTurtle <emmanuelrafol@gmail.com>
2024-06-10 19:39:06 +0800

    added more records for DrugInventory and DrugInventoryRecord in adddata.sql in dbconfig folder

--- db_config/adddata.sql
+++ db_config/adddata.sql
@@ -1,4 +1,4 @@
-INSERT INTO Account(AccountId, AccountName, AccountPassword, AccountEmail, AccountCreationDate) VALUES
+﻿INSERT INTO Account(AccountId, AccountName, AccountPassword, AccountEmail, AccountCreationDate) VALUES
 ('ACC0001', 'User1', 'password1', 'user1@mail.com', 1718096320), 
 ('ACC0002', 'User2', 'password2', 'user2@mail.com', 1717839920),
 ('ACC0003', 'User3', 'password3', 'user3@mail.com', 1717773120),
@@ -59,11 +59,42 @@ INSERT INTO Payments (PaymentId, AppointmentId, PaymentAmount, PaymentStatus) VA
 INSERT INTO DrugInventory(DrugName, DrugPrice, DrugDescription) VALUES
 ('Aspirin', 5.99, 'Pain relief'),
 ('Paracetamol', 5.99, 'Fever relief'),
-('Ibuprofen', 7.99, 'Inflammation relief');
+('Ibuprofen', 7.99, 'Inflammation relief'),
+('Panadol', 8.99, 'Pain relief'),
+('Calcium Channel Blockers', 16.99, 'Lower blood pressure'),
+('Tamoxifen', 7.99, 'Treatment for breast cancer'),
+('​​Penicillamine', 7.99, 'Treatment for rheumatoid arthritis'),
+('​Prazosin', 7.99, 'Treatment for hypertension'),
+('​Magnesium Carbonate', 7.99, 'Reducing excessive acid in the stomach'),
+('Promethazine', 7.99, 'Inflammation relief'),
+('​Aciclovir', 7.99, 'Treatment for viral infections'),
+('Clindamycin', 7.99, 'Treatment for viral infections'),
+('Co-trimoxazole', 7.99, 'Treatment for bacterial infections'), 
+('Metronidazole', 7.99, 'Treatment for gut infections'), 
+('Benzydamine', 7.99, 'Pain relief for sore throat'),  
+('Digoxin', 7.99, 'Treatment for heart failure'),  
+('Dipyridamole', 7.99, 'Prevent blood clots'), 
+('Pheniramine', 7.99, 'Pain relief for eyes'); 
+
 
 INSERT INTO DrugInventoryRecord (DrugRecordId, DrugName, DrugExpiryDate, DrugAvailableQuantity, DrugTotalQuantity, DrugRecordEntryDate) VALUES
 ('DRI0001', 'Aspirin', '2024-12-31', 100, 200, '2024-01-01'),
-('DRI0002', 'Ibuprofen', '2024-11-30', 150, 300, '2024-01-10');
+('DRI0002', 'Ibuprofen', '2024-11-30', 150, 300, '2024-01-10'),
+('DRI0003', 'Paracetemol', '2025-2-21', 150, 300, '2024-01-11'),
+('DRI0004', 'Panadol', '2024-11-30', 150, 300, '2024-01-11'),
+('DRI0005', 'Calcium Channel Blockers', '2024-11-30', 150, 300, '2024-01-12'),
+('DRI0006', 'Tamoxifen', '2024-10-23', 150, 300, '2024-01-12'),
+('DRI0007', 'Penicillamine', '2024-11-30', 150, 300, '2024-01-13'),
+('DRI0008', 'Prazosin', '2024-11-30', 150, 300, '2024-01-13'),
+('DRI0009', 'Magnesium Carbonate', '2024-11-30', 150, 300, '2024-01-14'),
+('DRI0010', 'Promethazine', '2024-11-30', 150, 300, '2024-01-14'),
+('DRI0011', 'Co-trimoxazole', '2024-11-30', 150, 300, '2024-01-15'), 
+('DRI0012', 'Metronidazole', '2024-11-30', 150, 300, '2024-01-15'), 
+('DRI0013', 'Benzydamine', '2024-11-30', 150, 300, '2024-01-16'), 
+('DRI0014', 'Digoxin', '2024-11-30', 150, 300, '2024-01-16'), 
+('DRI0015', 'Dipyridamole', '2024-11-30', 150, 300, '2024-01-17'), 
+('DRI0016', 'Pheniramine', '2024-11-30', 150, 300, '2024-01-18'), 
+('DRI0017', 'Aciclovir', '2024-11-30', 150, 300, '2024-01-19'); 
 
 INSERT INTO PrescribedMedication (PrescribedMedId, AppointmentId, DrugName, Quantity, Price, Reason, DrugRequest) VALUES
 ('PRM0001', 'APP0001', 'Aspirin', 10, 59.90, 'Pain relief', 'Completed'),
