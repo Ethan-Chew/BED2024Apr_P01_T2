b2201c66cb301c521b46ff9a1f533428b436fb75
Ethan-Chew <ethanchewmh@gmail.com>
2024-07-01 16:51:44 +0800

    chore: disable email notifications in development environment
    So I do not exceed the limit :)

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,4 +1,5 @@
 const Mail = require("../models/mail");
+require("dotenv").config();
 
 // Created by: Ethan Cchew
 const sendPaymentConfirmation = async (req, res) => {
@@ -9,15 +10,22 @@ const sendPaymentConfirmation = async (req, res) => {
         const appointmentDate = req.body.appointmentDate;
         const appointmentTime = req.body.appointmentTime;
 
-        const sendRequest = await Mail.sendPaymentConfirmation(recepient, paymentAmount, cardMerchant, appointmentDate, appointmentTime);
-        
-        if (sendRequest) {
+        // TODO: REMOVE BEFORE SUBMISSION
+        if (process.env.ENVIRONMENT !== "dev") {
+            const sendRequest = await Mail.sendPaymentConfirmation(recepient, paymentAmount, cardMerchant, appointmentDate, appointmentTime);
+            
+            if (sendRequest) {
+                res.status(201).json({
+                    message: "Payment Confirmation Email Sent"
+                });
+            }
+        } else {
             res.status(201).json({
-                message: "Payment Confirmation Email Sent"
+                message: "Payment Confirmation Email Sent",
+                info: "Email Sending Disabled in Development"
             });
         }
 
-        res.status(201).send();
     } catch (err) {
         console.error(err);
         res.status(500).json({
