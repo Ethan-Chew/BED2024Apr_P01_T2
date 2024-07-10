8596df0d3c124ced4be8cea8ca235f23505e8909
Jefflyl98 <jeffersonlyl98@gmail.com>
2024-07-11 01:28:42 +0800

    Added a batch file to create the SQL DB automatically

---
+++ db_config/run_all.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal
+
+REM Use PowerShell to get the computer name and combine with SQLEXPRESS
+for /f "usebackq tokens=*" %%a in (`powershell -Command "Write-Output \"$env:COMPUTERNAME\SQLEXPRESS\""`) do (
+    set SERVER_NAME=%%a
+)
+
+echo Server Name: %SERVER_NAME%
+
+echo Running DropCreateTable.sql
+sqlcmd -S %SERVER_NAME% -E -i "DropCreateTable.sql"
+
+echo Running createtables.sql
+sqlcmd -S %SERVER_NAME% -E -i "createtables.sql"
+
+echo Running adddata.sql
+sqlcmd -S %SERVER_NAME% -E -i "adddata.sql"
+
+echo All scripts executed successfully.
+pause
