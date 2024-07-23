8873dd4d3060b2337dd19d756f8af0186f7c8864
KurvyHerv <hervsie@gmail.com>
2024-07-23 15:53:42 +0800

    wip

--- models/2FAModel.js
+++ models/2FAModel.js
@@ -1,21 +1,64 @@
 const qrcode = require('qrcode');
 const otplib  = require('otplib');
+const sql = require("mssql");
+const dbConfig = require("../dbConfig");
 
-function generateQRCodeUrl(userEmail) {
-  const service = 'CareLinc';
-  const secret = otplib.authenticator.generateSecret();
-  const otpauth = otplib.authenticator.keyuri(userEmail, service, secret);
+class AccountSecret {
+  constructor(accountId, secret, userEmail) {
+    this.accountId = accountId;
+    this.secret = secret;
+    this.userEmail = userEmail;
+  }
 
-  return new Promise((resolve, reject) => {
-    qrcode.toDataURL(otpauth, (err, url) => {
-      if (err) {
-        console.error('Error with QR', err);
-        reject(err);
-      } else {
-        resolve(url);
-      }
+  static async getAuth(accountId) {
+    const connection = await sql.connect(dbConfig);
+    const query = `
+    SELECT * FROM AccountSecret WHERE AccountId = @AccountId;
+    `;
+
+    const request = connection.request();
+    request.input('AccountId', accountId);
+
+    const result = await request.query(query);
+    connection.close();
+
+    return result.recordset[0];
+  }
+
+  static async add2FA(accountId, userEmail) {
+
+    const service = 'CareLinc';
+    const secret = otplib.authenticator.generateSecret();
+    const otpauth = otplib.authenticator.keyuri(userEmail, service, secret);
+
+    const connection = await sql.connect(dbConfig);
+    const query = `
+    INSERT INTO AccountSecret (AccountId, SecretKey) VALUES
+    (@AccountId, @SecretKey);
+    `;
+
+    const request = connection.request();
+    request.input('AccountId', accountId);
+    request.input('SecretKey', secret);
+
+    const result = await request.query(query);
+    connection.close();
+
+    return new Promise((resolve, reject) => {
+      qrcode.toDataURL(otpauth, (err, url) => {
+        if (err) {
+          console.error('Error with QR', err);
+          reject(err);
+        } else {
+          resolve(url);
+        }
+      });
     });
-  });
+
+  }
+  
 }
 
-module.exports = { generateQRCodeUrl };
+module.exports = { 
+  AccountSecret 
+};
--- public/scripts/admin/2FAHandler.js
+++ public/scripts/admin/2FAHandler.js
@@ -1,16 +1,40 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const response = await fetch('/api/generateQRCode', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Email': localStorage.getItem('email'),
-        },
-    });
+
+    const response = await fetch(`/api/getAuth/${sessionStorage.getItem('accountId')}`)
 
     if (response.ok) {
-        const imageUrl = await response.text();
-        document.getElementById('qrcode').src = imageUrl;
-    } else {
-        console.error('Failed to load QR code', await response.text());
+        console.log(response);
+    }
+    else {
+        response = await fetch('/api/generateQRCode', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Email': sessionStorage.getItem('email'),
+                'AccountId': sessionStorage.getItem('accountId'),
+            },
+        });
+    
+        if (response.ok) {
+            const imageUrl = await response.text();
+            document.getElementById('qrcode').src = imageUrl;
+        } else {
+            console.error('Failed to load QR code', await response.text());
+        }
+    
+        const popup = document.getElementById('popup');
+        if (popup) {
+            popup.style.display = 'flex';
+        }
+        const overlay = document.querySelector('.fixed.inset-0.bg-black.bg-opacity-50');
+        popup = document.querySelector('.bg-white.w-full.max-w-lg.mx-auto');
+    
+        overlay.addEventListener('click', function(event) {
+            // Check if the click was outside the popup content
+            if (!popup.contains(event.target)) {
+                // Close the popup by hiding the overlay
+                overlay.style.display = 'none';
+            }
+        });
     }
 });
